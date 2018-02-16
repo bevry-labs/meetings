b6c6929b1e447cd927c0dd6fa44219a33fcbaca9
Benjamin Lupton <b@lupton.cc>
2018-02-17 07:08:43 +0800

    redirects are now gitignored, so they only exist at compile time - renamed study-group-calendar to calendar

--- scripts/compile.js
+++ scripts/compile.js
@@ -1,8 +1,9 @@
 const path = require('path')
-const fs = require('fs')
-const mkdirp = require('mkdirp')
+const fs = require('then-fs')
+const mkdirp = require('mkdirp-promise')
 
-function renderTemplate (url, title = 'Redirect') {
+// Redirect Template
+function renderRedirectTemplate (url, title = 'Redirect') {
 	return `<!DOCTYPE html>
 <html>
 
@@ -24,16 +25,41 @@ function renderTemplate (url, title = 'Redirect') {
 </html>`
 }
 
-const redirects = require('../package.json').redirects
-const www = path.join(__dirname, '..', 'www')
+// Async Helper
+async function init () {
+	// Fetch paths
+	const packagePath = path.join(__dirname, '..', 'package.json')
+	const wwwPath = path.join(__dirname, '..', 'www')
+	const gitignorePath = path.join(__dirname, '..', '.gitignore')
 
-Object.keys(redirects).forEach(function (key) {
-	const url = redirects[key]
-	const dir = path.join(www, key)
-	mkdirp(dir, function (err) {
-		if (err) throw err
-		fs.writeFile(path.join(dir, 'index.html'), renderTemplate(url), function (err) {
-			if (err) throw err
+	// Fetch redirect data
+	const redirects = require(packagePath).redirects
+	const keys = Object.keys(redirects).sort()
+
+	// Update gitignore with redirect directories
+	const gitignoreContentsOriginal = await fs.readFile(gitignorePath, 'utf8')
+	const gitignoreContentsUpdated =
+		gitignoreContentsOriginal.split('# CUSTOM')[0]
+		+ '# CUSTOM\n\n'
+		+ keys.map((key) => `www/${key}/`).join('\n')
+		+ '\n'
+	await fs.writeFile(gitignorePath, gitignoreContentsUpdated)
+
+	// Create the redirect files
+	return Promise.all(
+		keys.map(function (key) {
+			const url = redirects[key]
+			const dir = path.join(wwwPath, key)
+			const file = path.join(dir, 'index.html')
+			const contents = renderRedirectTemplate(url)
+			return mkdirp(dir).then(() => fs.writeFile(file, contents))
 		})
+	)
+}
+
+// Run
+init()
+	.then(() => console.log('success'))
+	.catch(function (err) {
+		throw err
 	})
-})
