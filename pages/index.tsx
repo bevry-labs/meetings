32ade276ac900b0a95df8b1fead226b7091f5bde
Benjamin Lupton <b@lupton.cc>
2018-12-28 00:22:51 +1100

    make link listing and redirects programmatic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { filter } from '../data/links'
 export default () => (
 	<div>
 		<Head>
@@ -7,58 +8,17 @@ export default () => (
 				name="keywords"
 				content="jordan b peterson, jbp, jordan peterson, peterson, psychology, philosophy, maps of meaning, psychological signficance of the biblical stories, bible, community, fans, study group, lecture notes, reading group, forum, discussion"
 			/>
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
 			<link rel="stylesheet" href="//unpkg.com/normalize.css/normalize.css" />
 			<link rel="stylesheet" href="/static/style.css" />
 		</Head>
 		<h1>Jordan B Peterson Community</h1>
 		<ul>
-			<li>
-				<a href="/newsletter/">Newsletter</a>
-			</li>
-			<li>
-				<a href="/calendar/">Calendar</a>
-			</li>
-			<li>
-				<a href="/study-group/">Study Group</a>
-			</li>
-			<li>
-				<a href="/lecture-notes/">Lecture Notes</a>
-			</li>
-			<li>
-				<a href="/reading-group/">Reading Group</a>
-			</li>
-			<li>
-				<a href="/forum/">Discussion Forum</a>
-			</li>
-			<li>
-				<a href="/youtube/">YouTube</a>
-			</li>
-			<li>
-				<a href="/twitter/">Twitter</a>
-			</li>
-			<li>
-				<a href="/reddit/">Reddit</a>
-			</li>
-			<li>
-				<a href="/meetups/">Meetups</a>
-			</li>
-			<li>
-				<a href="/discord/">Discord</a>
-			</li>
-			<li>
-				<a href="/facebook-liberal-group/">
-					Facebook Group: Liberal Discussion Group
-				</a>
-			</li>
-			<li>
-				<a href="/facebook-study-group/">Facebook Group: Study Group</a>
-			</li>
-			<li>
-				<a href="/source/">Source Code for this Website</a>
-			</li>
-			<li>
-				<a href="/feedback/">Website Discussion</a>
-			</li>
+			{filter('home').map(link => (
+				<li key={link.id}>
+					<a href={link.url}>{link.text}</a>
+				</li>
+			))}
 		</ul>
 	</div>
 )
