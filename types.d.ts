54d59c1fe49b72bb7769706336f9209b97e34342
Benjamin Lupton <b@lupton.cc>
2018-12-30 05:03:39 +1100

    dev: update hooks for richer event cycle
    do not publish this, as events are dev
    
    todo, figure out how to get the listing component to update

--- types.d.ts
+++ types.d.ts
@@ -1,11 +1,18 @@
 import { calendar_v3 } from 'googleapis'
+import { Moment } from 'moment'
 
 type Time = { dateTime: string; timeZone: string }
-export interface EventType extends calendar_v3.Schema$Event {
+export interface RawEventType extends calendar_v3.Schema$Event {
 	start: Time
 	end: Time
 	originalStartTime: Time
 }
-export type EventsType = EventType[]
+export type RawEventsType = RawEventType[]
+
+export type RichEventType = RawEventType & {
+	start: Moment
+	end: Moment
+}
+export type RichEventsType = RichEventType[]
 
 export type Children = Array<string | JSX.Element>
