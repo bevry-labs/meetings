4af689eebd37ee58452c98530b20aa5d83c0c5f8
Benjamin Lupton <b@lupton.cc>
2019-01-05 11:32:38 +1100

    trim unuset daet code

--- shared/daet.ts
+++ shared/daet.ts
@@ -1,39 +1,16 @@
-import { number } from 'prop-types'
+/* eslint no-dupe-class-members:0, no-throw-literal:0, no-case-declarations:0 */
 import * as formatMessage from 'format-message'
 
-/* eslint no-dupe-class-members:0, no-throw-literal:0, no-case-declarations:0 */
-
-type CustomMessageOptions = { now: Daet; seconds: number }
-
+type UnitsToHour = 'millisecond' | 'second' | 'minute' | 'hour'
+type UnitsToDay = 'millisecond' | 'second' | 'minute' | 'hour' | 'day'
 type Input = string | number | Date
-type Unit = 'millisecond' | 'second' | 'minute' | 'hour' | 'day'
-/*
-type Tier =
-	| {
-			singular?: string
-			plural: string
-			unit: number
-			limit: number
-	  }
-	| {
-			custom: (options: CustomMessageOptions) => string
-			unit: number
-			limit: number
-	  }
-	*/
 
-/*
-type Parts = {
-	year: number
-	month: number
-	date: number
-	day: number
-	hour: number
-	minute: number
-	second: number
-	time: number
+interface Tier {
+	limit?: number
+	when?: number
+	message?: string
+	custom?: (delta: number, when: Daet) => string
 }
-*/
 
 export const Millisecond = 1
 export const Second = Millisecond * 1000
@@ -41,26 +18,6 @@ export const Minute = Second * 60
 export const Hour = Minute * 60
 export const Day = Hour * 24
 export const Week = Day * 7
-
-/*
-const tiers: Tiers = [
-	{ unit: 0, limit: Second, plural: 'right now' },
-	{
-		unit: Second,
-		limit: Minute,
-		singular: '%d second',
-		plural: '%d seconds'
-	},
-	{
-		unit: Minute,
-		limit: Hour,
-		singular: '%d minute',
-		plural: '%d minutes'
-	},
-	{ unit: Hour, limit: Day, singular: '%d hour', plural: '%d hours' },
-	{ unit: Day, limit: Infinity, singular: '%d day', plural: '%d days' }
-]
-*/
 
 const intl = {
 	now: 'right now',
@@ -93,36 +50,6 @@ const intl = {
 	yesterday: `yesterday`,
 	thisWeek: `this {value}`,
 	nextWeek: `next {value}`
-}
-
-/*
-function sp(value: number, singular: string, plural: string) {
-	return value ? (value + ' ' + (value % 2) ? plural : singular) : ''
-}
-*/
-
-/*
-type TierTime =
-	| {
-			limit: number
-	  }
-	| {
-			when: number
-	  }
-type TierMessage =
-	| {
-			message: string
-	  }
-	| {
-			custom: (delta: number) => string
-	  }
-type Tier = TierTime & TierMessage
-*/
-interface Tier {
-	limit?: number
-	when?: number
-	message?: string
-	custom?: (delta: number, when: Daet) => string
 }
 
 formatMessage.setup({
@@ -203,10 +130,10 @@ export default class Daet {
 	constructor(input?: Input) {
 		this.raw = input ? new Date(input) : new Date()
 	}
-	minus(value: number, unit: Unit): Daet {
+	minus(value: number, unit: UnitsToDay): Daet {
 		return this.add(value * -1, unit)
 	}
-	add(value: number, unit: Unit): Daet {
+	add(value: number, unit: UnitsToDay): Daet {
 		switch (unit) {
 			case 'millisecond': {
 				const next = new Date(this.raw.getTime() + value)
@@ -233,7 +160,7 @@ export default class Daet {
 	private rawClone() {
 		return new Date(this.raw)
 	}
-	set(value: number, unit: 'millisecond' | 'second' | 'minute' | 'hour'): Daet {
+	set(value: number, unit: UnitsToHour): Daet {
 		switch (unit) {
 			case 'millisecond': {
 				const next = this.rawClone().setMilliseconds(value)
@@ -257,7 +184,7 @@ export default class Daet {
 				throw new Error('unknown unit')
 		}
 	}
-	reset(unit: 'millisecond' | 'second' | 'minute' | 'hour'): Daet {
+	reset(unit: UnitsToHour): Daet {
 		switch (unit) {
 			case 'millisecond': {
 				const next = this.rawClone().setMilliseconds(0)
@@ -305,9 +232,7 @@ export default class Daet {
 	fromNow(): string {
 		const delta = Math.abs(this.getDelta())
 		const time = this.getRoundedTime()
-		console.log('--', delta, '-------------')
 		const tiers = Daet.tiers
-		console.log('--', delta, '-------------')
 		for (const tier of tiers) {
 			const limit = !tier.limit || delta < tier.limit
 			const when = !tier.when || time < tier.when
@@ -324,50 +249,6 @@ export default class Daet {
 		}
 		throw new Error('no tier matched the input delta')
 	}
-	/*
-	fromNow(): string {
-		const delta = Math.abs(this.getDeltaSeconds())
-		const tiers = this.tiers
-		console.log('--', delta, '-------------')
-		for (const { unit, limit } of tiers) {
-			const within = delta < limit
-			if (within) {
-				const unitDelta = Math.ceil(delta / unit)
-				const message = unitDelta > 1 ? plural : singular
-				console.log(delta, limit, within, unit, unitDelta, message)
-				return message.replace('%d', unitDelta.toString())
-			}
-		}
-		throw new Error('no tier matched the input delta')
-	}
-	getParts(): Parts {
-		const raw = this.raw
-		return {
-			year: raw.getFullYear(),
-			month: raw.getMonth(),
-			date: raw.getDate(),
-			day: raw.getDay(),
-			hour: raw.getHours(),
-			minute: raw.getMinutes(),
-			second: raw.getSeconds(),
-			time: raw.getTime()
-		}
-	}
-	diff(B: Daet): Parts {
-		const a = this.getParts()
-		const b = B.getParts()
-		return {
-			year: a.year - b.year,
-			month: a.month - b.month,
-			date: a.date - b.date,
-			day: a.day - b.day,
-			hour: a.hour - b.hour,
-			minute: a.minute - b.minute,
-			second: a.second - b.second,
-			time: a.time - b.time
-		}
-	}
-	*/
 	toISOString(): string {
 		return this.raw.toISOString()
 	}
@@ -375,8 +256,3 @@ export default class Daet {
 		return this.raw.toJSON()
 	}
 }
-
-/*
-import Daet from './shared/Daet'
-Daet.create().add(30, 'second').getSecondsDelta()
-*/
