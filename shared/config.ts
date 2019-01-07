f2ec42abf2bd4a9a93506ae84dee9092486f6fca
Benjamin Lupton <b@lupton.cc>
2019-01-07 11:03:38 +1100

    try and trim down now env size

--- shared/config.ts
+++ shared/config.ts
@@ -2,3 +2,5 @@ export const DEVELOPMENT = false
 export const EVENTS_URL = 'https://jordanbpeterson.community/api/events/'
 export const expiresValue = 3
 export const expiresUnit = 'hour'
+export const GOOGLE_CALENDAR_ID =
+	'bevry.me_qeq66am9eji1bcll7ntrn3u830@group.calendar.google.com'
