8c4fb46452a0c1f44d8ef2bb857042cb71839f45
Benjamin Lupton <b@lupton.cc>
2019-01-07 12:48:44 +1100

    renamed layout to page, as that is what it is

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,41 @@
 import { filter } from '../shared/links'
 import Link from '../client/components/link'
-import Layout from '../client/components/layout'
+import Page from '../client/components/page'
 import Events from '../client/components/events'
-import { DisplayText } from '@shopify/polaris'
+import { DisplayText, List, Layout } from '@shopify/polaris'
 import { RawEventsType, enrichEvents, fetchRawEvents } from '../shared/events'
 
 type Props = {
 	rawEvents: RawEventsType
 }
 
-const Page = ({ rawEvents }: Props) => {
+const IndexPage = ({ rawEvents }: Props) => {
 	const events = enrichEvents(rawEvents)
 	return (
-		<Layout>
-			<DisplayText size="small">
-				The Jordan B Peterson Community is a fan-led initiative of a{' '}
-				<Link id="study-group" />, <Link id="reading-group" />,{' '}
-				<Link id="lecture-notes" />, and <Link id="podcast" text="Podcast" />.
-			</DisplayText>
+		<Page>
+			<Layout.Section>
+				<DisplayText size="small">
+					The Jordan B Peterson Community is a fan-led initiative of a{' '}
+					<Link id="study-group" />, <Link id="reading-group" />,{' '}
+					<Link id="lecture-notes" />, and <Link id="podcast" text="Podcast" />.
+				</DisplayText>
+			</Layout.Section>
 			{events.length ? <Events events={events} /> : ''}
-			<ul>
-				{filter('home').map(link => (
-					<li key={link.id}>
-						<Link id={link.id} />
-					</li>
-				))}
-			</ul>
-		</Layout>
+			<Layout.Section>
+				<List>
+					{filter('home').map(link => (
+						<List.Item key={link.id}>
+							<Link id={link.id} />
+						</List.Item>
+					))}
+				</List>
+			</Layout.Section>
+		</Page>
 	)
 }
 
-Page.getInitialProps = function({ req }: { req: any }): Promise<Props> {
+IndexPage.getInitialProps = function({ req }: { req: any }): Promise<Props> {
 	return fetchRawEvents().then(rawEvents => ({ rawEvents }))
 }
 
-export default Page
+export default IndexPage
