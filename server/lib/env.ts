5f603cbcff42e68c37e6069d6c765484dba1ae1e
Benjamin Lupton <b@lupton.cc>
2019-01-07 11:15:38 +1100

    remove base64 encoding to try and trim down size

--- server/lib/env.ts
+++ server/lib/env.ts
@@ -4,18 +4,10 @@ export function getEnv(key: string): any {
 	return value
 }
 
-export function fromBase64(value: string): string {
-	return Buffer.from(value, 'base64').toString('ascii')
-}
-
-export function toBase64(value: string): string {
-	return Buffer.from(value, 'ascii').toString('base64')
-}
-
 export function parseEnv<T extends Object>(value: string): T {
-	return JSON.parse(fromBase64(value))
+	return JSON.parse(value)
 }
 
 export function createEnv(value: Object): string {
-	return toBase64(JSON.stringify(value))
+	return JSON.stringify(value)
 }
