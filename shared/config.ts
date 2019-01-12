451b70bff300e9df2252bc6f19a33a0b553a30a3
Benjamin Lupton <b@lupton.cc>
2019-01-12 11:17:01 +1100

    specific join url for this week

--- shared/config.ts
+++ shared/config.ts
@@ -1,6 +1,7 @@
 export const DEVELOPMENT = process.env.NODE_ENV === 'development'
 export const eventsUrl = 'https://jordanbpeterson.community/api/events/'
-export const podcastJoinUrl = 'https://jordanbpeterson.community/podcast-join/'
+export const podcastJoinUrl =
+	'https://hangouts.google.com/hangouts/_/eae3j7t25rfzleuect5y7iui5ye' // 'https://jordanbpeterson.community/podcast-join/'
 export const podcastWatchUrl = 'https://www.youtube.com/watch?v=w4sXo_kBtzs' // 'https://jordanbpeterson.community/youtube/'
 export const expiresValue = 3
 export const expiresUnit = 'hour'
