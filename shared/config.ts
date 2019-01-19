53f32663a7f6a637f5d831cf67851280f07ab0c4
Benjamin Lupton <b@lupton.cc>
2019-01-19 11:48:28 +1100

    this weeks podcast urls

--- shared/config.ts
+++ shared/config.ts
@@ -1,8 +1,8 @@
 export const DEVELOPMENT = process.env.NODE_ENV === 'development'
 export const eventsUrl = 'https://jordanbpeterson.community/api/events/'
 export const podcastJoinUrl =
-	'https://hangouts.google.com/hangouts/_/eae3j7t25rfzleuect5y7iui5ye' // 'https://jordanbpeterson.community/podcast-join/'
-export const podcastWatchUrl = 'https://www.youtube.com/watch?v=w4sXo_kBtzs' // 'https://jordanbpeterson.community/youtube/'
+	'https://hangouts.google.com/hangouts/_/7coz2sbx5rfwdfdzrnmrgltrtye' // 'https://jordanbpeterson.community/podcast-join/'
+export const podcastWatchUrl = 'https://www.youtube.com/watch?v=6Buc3omnhpA' // 'https://jordanbpeterson.community/youtube/'
 export const expiresValue = 3
 export const expiresUnit = 'hour'
 export const googleCalendarId =
