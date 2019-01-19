09a28b183ed26b1be5eb53ad51009fe1c3bfd2f9
Benjamin Lupton <b@lupton.cc>
2019-01-19 13:29:53 +1100

    make sure we show both next weeks sessions

--- server/api/events.ts
+++ server/api/events.ts
@@ -7,7 +7,7 @@ import { expiresValue, expiresUnit } from '../../shared/config'
 async function processEvents() {
 	const now = new Daet()
 	const start = now.minus(expiresValue, expiresUnit)
-	const finish = now.plus(1, 'week')
+	const finish = now.plus(1, 'week').plus(1, 'day')
 	const events = await getEvents(start.toISOString(), finish.toISOString())
 	return events
 }
