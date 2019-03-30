62a07ef723a91404b86936894f7d75d7943b582a
Benjamin Lupton <b@lupton.cc>
2019-03-30 11:50:38 +1100

    update public session links for this week

--- shared/config.ts
+++ shared/config.ts
@@ -2,9 +2,9 @@ export const DEVELOPMENT = process.env.NODE_ENV === 'development'
 export const eventsUrl = 'https://meet.bevry.me/api/events/'
 
 export const podcastJoinUrl =
-	'https://hangouts.google.com/hangouts/_/w7ojei33xfccfobg3umi6yqwgie'
+	'https://hangouts.google.com/hangouts/_/hvmfmb2wybeu7iueuu5z3u742ue'
 
-export const podcastWatchUrl = 'https://www.youtube.com/watch?v=Rsj6tygDoSI'
+export const podcastWatchUrl = 'https://www.youtube.com/watch?v=dMiInyTp82U'
 
 export const expiresValue = 3
 export const expiresUnit = 'hour'
