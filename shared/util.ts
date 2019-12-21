a526564054731838e6574d9cd7b6dd4867572790
Benjamin Lupton <b@lupton.cc>
2019-12-21 20:06:40 +0800

    fix index page not working when linked

--- shared/util.ts
+++ shared/util.ts
@@ -6,8 +6,7 @@ export function getHostname(req?: IncomingMessage): string {
 	// we could just use '' on client-side, which will respect ports better, but this is fine
 	// because `/api/events` is the same on client-side as `https://hostname/api/events`
 	// a href="/api/events" => "https://hostname/api/events"
-	const host =
-		(req && req.headers.host) || (location && location.hostname) || ''
-	if (!host) throw new Error('failed to identify the hostname')
+	const host = (req && req.headers.host) || (location && location.host) || ''
+	if (!host) throw new Error('failed to identify the host')
 	return host.startsWith('localhost') ? `http://${host}` : `https://${host}`
 }
